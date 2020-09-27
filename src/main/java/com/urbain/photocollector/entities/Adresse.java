8328f69ad08484709a1777e700dc152c1866f12d
Koné <bkone.m2i@gmail.com>
2020-09-27 20:11:18 +0200

    update adresse entity

--- src/main/java/com/urbain/photocollector/entities/Adresse.java
+++ src/main/java/com/urbain/photocollector/entities/Adresse.java
@@ -1,5 +1,44 @@
 package com.urbain.photocollector.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Table(name = "adresse")
+@Entity
 public class Adresse {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long adresse_id;
+
+	@Column(length = 10, nullable = false)
+	private Long num;
+
+	@Enumerated(EnumType.STRING)
+	private VoieEnum voie;
+
+	@Column(nullable = false)
+	private String complement_voie;
+
+	@Column(nullable = false)
+	private String ville;
+
+	@Column(length = 10, nullable = false)
+	private int code_postal;
+
+	@Column(length = 100, nullable = false)
+	private String pays;
+
+	@Column(length = 50)
+	private String nom_adresse;
+
+	@ManyToOne
+	private Utilisateur utilisateur;
 
 }
